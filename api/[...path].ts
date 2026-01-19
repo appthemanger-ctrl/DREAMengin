8bc42811a93cbf5e556217f39322ccc45bd34dd4
appthemanger-ctrl <253588904+appthemanger-ctrl@users.noreply.github.com>
2026-01-19 15:31:23 -0800

    delte

--- api/[...path].ts
+++
@@ -1,9 +0,0 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { createApp } from "../server/app";
-
-const app = createApp();
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  // Express is compatible with Vercel's Node req/res
-  return (app as any)(req, res);
-}
--- api/index.ts
+++
@@ -1,7 +0,0 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { getApp } from "./_handler";
-
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const app = await getApp();
-  return (app as any)(req, res);
-}
