26d07da302c492e584b504ace2561de76a5a6db3
appthemanger-ctrl <253588904+appthemanger-ctrl@users.noreply.github.com>
2026-01-23 01:26:07 -0800

    main

--- modules/widgets/messages/index.js
+++
@@ -1,9 +0,0 @@
-'use client';
-export default function MessagesWidget(){
-  return (
-    <div>
-      <div className="text-sm opacity-70">Messages</div>
-      <div className="font-medium">Inbox coming soon</div>
-    </div>
-  );
-}
--- modules/widgets/promo/index.js
+++
@@ -1,9 +0,0 @@
-'use client';
-export default function PromoWidget(){
-  return (
-    <div>
-      <div className="text-sm opacity-70">Promo</div>
-      <div className="font-medium">Add your promo in Settings → Widgets</div>
-    </div>
-  );
-}
