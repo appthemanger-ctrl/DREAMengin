4609bfe42b6ca1486b39f2700c36fe2c68d244aa
Jose Mancilla <253588904+appthemanger-ctrl@users.noreply.github.com>
2026-02-05 07:53:06 -0800

    Create next.config.js with Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,18 +1,14 @@
 'use strict';
 
-const withTM = require('next-transpile-modules')([]);
-
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ **/
 const nextConfig = {
   reactStrictMode: true,
-  typescript: {
-    // Setting this to false allows production builds to successfully complete even if
-    // your project has type errors.
-    ignoreBuildErrors: false,
-  },
-  build: {
-    // Build options go here
+  swcMinify: true,
+  images: {
+    domains: ['example.com'], // Replace with your image domains
   },
 };
 
-module.exports = withTM(nextConfig);
+module.exports = nextConfig;
