c1f6ff8131869a8c23a4226d9522be1b7e8033ef
Jose Mancilla <253588904+appthemanger-ctrl@users.noreply.github.com>
2026-02-05 07:51:07 -0800

    Create dashboard component

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="dashboard">
+    <h1>Dashboard</h1>
+    <div class="widgets">
+      <!-- Widgets can be placed here -->
+      <widget></widget>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Dashboard'
+}
+</script>
+<style>
+.dashboard { /* Dashboard styling */ }
+</style>
