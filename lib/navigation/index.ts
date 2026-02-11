a6ab2e30ac7a4746b796c6bd09340f2abe47a90d
Jose Mancilla <253588904+appthemanger-ctrl@users.noreply.github.com>
2026-02-10 22:18:38 -0800

    Revert "Implement AnchorWidget: gesture-driven navigation with O(1) mode switching"

--- lib/navigation/index.ts
+++ lib/navigation/index.ts
@@ -8,8 +8,6 @@ export { TransformSolver } from './TransformSolver';
 export { WidgetInstanceMemory, WidgetPresentation, WidgetVisibility, WidgetBindingType } from './WidgetInstanceMemory';
 export { SpatialNavigationEngine } from './SpatialNavigationEngine';
 export { useNavigation } from './useNavigation';
-export { AnchorStateBuffer, MODE_HOME, MODE_PROFILE, MODE_SHRUNK, HOLD_IDLE, HOLD_HOLDING, HOLD_FIRED } from './AnchorStateBuffer';
-export { AnchorWidgetStorage } from './AnchorWidgetStorage';
 
 export type { PointerState } from './PointerEventCapture';
 export type { GestureFrame } from './GestureFrameComputer';
@@ -18,4 +16,3 @@ export type { TransformOutput, ViewportMetrics } from './TransformSolver';
 export type { WidgetTransformState, WidgetInstanceRecord } from './WidgetInstanceMemory';
 export type { EngineConfig, EngineEventType, EngineEventCallback } from './SpatialNavigationEngine';
 export type { UseNavigationOptions, NavigationState } from './useNavigation';
-export type { HomeSlotMapping, PriorityWidget, AnchorWidgetState } from './AnchorWidgetStorage';
